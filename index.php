fa010e7a74f201ac4bb2222fa1067d4679456c07
Mulyadi Oey <oey.mulyadi@gmail.com>
2013-06-27 15:41:56 -0700

    index.php [status: incomplete]. Can we start all over again? :)

---
+++ index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title>Siku-Siku</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta name="description" content="">
+	<meta name="author" content="">
+	
+	<link type="text/css" rel="stylesheet" href="http://fonts.googleapis.com/css?family=Gilda+Display|Raleway:500,700">
+	<!--
+	<link type="text/css" rel="stylesheet" href="/assets/css/bootstrap.css">
+	<link type="text/css" rel="stylesheet" href="/assets/css/bootstrap-responsive.css">
+	-->
+	<link type="text/css" rel="stylesheet" href="/assets/css/normalize.css">
+	<link type="text/css" rel="stylesheet" href="/assets/css/siku-siku.css">
+</head>
+<body>
+	<header role="document">
+		<a href="/"><img id="logo" src="/assets/img/siku-siku-logo.png" alt="Siku-Siku logo"></a>
+		<nav>
+			<ul>
+				<li><a href="/portfolio.html">Portfolio</a></li>
+				<li><a href="/about.html">About</a></li>
+			</ul>
+		</nav>
+	</header>
+	<section role="document">
+		<h1>Hello from Siku-Siku!</h1>
+		<p>Thanks for stopping by. We are a small web development studio run by Mulyadi Oey. We help start-ups, small / medium businesses, individuals, and digital agencies make websites and web applications.</p>
+		<p>We&rsquo;re based in San Francisco and specialize in design and implementation of user interface (UI) for the web. We love what we do. And, we&rsquo;d be delighted to see what we can do for you.</p>
+	</section>
+</body>
+</html>
